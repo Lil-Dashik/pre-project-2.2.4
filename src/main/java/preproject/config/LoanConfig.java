25481e61fb9f5ee2a26ddde7530856b00c07d0d5
Lil-Dashik <mors_ph_sadboy@mail.ru>
2025-03-07 13:51:10 +0400

    добавила свой стартер вместо IncomeClient

--- src/main/java/preproject/config/LoanConfig.java
+++ src/main/java/preproject/config/LoanConfig.java
@@ -3,9 +3,8 @@ package preproject.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
 
 
 @Getter
@@ -17,11 +16,4 @@ public class LoanConfig {
     private double maxCreditRateFromIncome;
     private double minCarPriceForLoan;
     private double maxCreditRateFromCarPrice;
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-
 }

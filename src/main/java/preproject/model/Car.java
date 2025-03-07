55af6b25120b6d7e7d9e32a8cacb4cb050c7e81c
Lil-Dashik <mors_ph_sadboy@mail.ru>
2025-03-07 12:08:42 +0400

    добавила свой стартер вместо IncomeClient

--- src/main/java/preproject/model/Car.java
+++ src/main/java/preproject/model/Car.java
@@ -1,8 +1,9 @@
 package preproject.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
-import javax.persistence.*;
+
 
 
 @Getter
--- src/main/java/preproject/service/IncomeService.java
+++ src/main/java/preproject/service/IncomeService.java
@@ -1,20 +1,26 @@
 package preproject.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import preproject.client.IncomeClient;
-import preproject.model.IncomeResponse;
+import starter.client.IncomeClient;
+import starter.client.IncomeResponse;
+
 
 import java.util.Arrays;
 import java.util.Objects;
 
-@RequiredArgsConstructor
 @Service
 public class IncomeService {
     private final IncomeClient incomeClient;
+
+    public IncomeService(IncomeClient incomeClient) {
+        this.incomeClient = incomeClient;
+    }
+
+    public IncomeResponse[] getIncomes() {
+        return incomeClient.fetchIncomes();
+    }
 
     public Double getUserIncome(Long userId) {
         return Arrays.stream(incomeClient.fetchIncomes())
@@ -22,6 +28,7 @@ public class IncomeService {
                 .map(IncomeResponse::getIncome)
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "The user with the ID " + userId + " was not found!"));
+
     }
 }
 
